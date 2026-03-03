cc73337e71c838d88ea30210f7a71dfaa39238fd
Rillu <gayerinst@gmail.com>
2026-03-03 18:44:00 +0800

    Added main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# Run the program here, this will be the main of the program
+
+# Welcome message to welcome user
+welcome_message = f"""
+{'-' * 50}
+Welcome to Paws & Play Hub management system!
+{'-' * 50}
+Menu: 
+1. Pet Owner Login
+2. Pet Owner Register
+3. Staff Login
+"""
+
+#Print the welcome message
+print(welcome_message)
+
+# User input for menu selection
+user_choice = int(input("Select a menu option: "))
