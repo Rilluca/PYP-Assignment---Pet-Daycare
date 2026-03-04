53cb5d8ee8fee2f4f7a987acb59f463f77a1d679
Crush <TP115115@mail.apu.edu.my>
2026-03-04 13:13:06 +0800

    Crush was here

--- src/main.py
+++ src/main.py
@@ -15,10 +15,38 @@ Menu:
 
 #Print the welcome message
 print(welcome_message)
-
 # User input for menu selection
 user_choice = int(input("Choose a menu option: "))
 
 match user_choice:
     case 2:
         auth.register_user()
+
+
+
+from data_manage import save_user_register
+
+
+def validate_username():
+    while True:
+        print("1. Register")
+        print("2. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            username = input("Enter your username: ")
+            password = input("Enter your password: ")
+            result = save_user_register(username, password)
+            if result:
+                print("User successfully registered!")
+
+        elif choice == "2":
+            print("The system has ended")
+            break
+
+        else:
+            print("Invalid choice")
+
+validate_username()
+
--- test.py
+++ test.py
@@ -1,1 +1,2 @@
 #Test your GitHub here! - Rillu
+#                       - Crush
