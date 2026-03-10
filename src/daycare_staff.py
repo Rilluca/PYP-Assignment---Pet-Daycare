1b6b078f1f1512e961b7743eadf36d2cbe30a040
Rillu <gayerinst@gmail.com>
2026-03-10 23:48:43 +0800

    Updated

--- src/daycare_staff.py
+++ src/daycare_staff.py
@@ -2,3 +2,49 @@
 # - Update pet care status (feeding, grooming, activities).
 # - Log daily care and grooming records.
 # - Generate service summary report.
+
+# Menu message for daycare/grooming staff
+daycare_staff_menu = f"""
+{'-' * 50}
+Daycare/Grooming Staff Menu
+{'-' * 50}
+What would you like to do today?
+1. Update care status
+2. Add daily care and grooming record
+3. Generate summary report
+4. Quit
+"""
+
+def show_daycare_staff_menu():
+    print(daycare_staff_menu)
+
+    while True:
+        try:
+            # Ask user for choice input
+            daycare_staff_choice = int(input("Enter your choice: "))
+
+            # Match case for menu selection
+            match daycare_staff_choice:
+                case 1:
+                    print("replace function to system admin here")
+                    break
+
+                case 2:
+                    print("replace function to booking officer here")
+                    break
+
+                case 3:
+                    print("abc")
+                    break
+
+                case 4:
+                    print("Quitting program now.")
+                    break
+
+                case _:
+                    print("Input can only be in number and within range, please try again.\n")
+
+        except ValueError:
+            print("Input can only be in number and within range, please try again.\n")
+
+
